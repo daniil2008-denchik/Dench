09c139fc6907b07fbb5778445a68d18e1a5cff09
daniil2008-denchik <daniilsalahutdinov36@gmail.com>
2026-05-23 21:33:06 +1000

    Project.cpp

--- 20260303-133935.cpp
+++ 20260303-133935.cpp
@@ -1,40 +1,119 @@
-#include <iostream>  
-#include <string>   
-using namespace std; 
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+#include <cmath>
 
+#ifdef _WIN32
+    #include <windows.h>
+    #include <conio.h>
+#else
+    #include <unistd.h>
+    #include <termios.h>
+    #include <fcntl.h>
+#endif
 
-struct student {
-    string name;  
-    int age;      
-    string role;  
-    string group;  
-};
+using namespace std;
 
+void logToFile(const string& message) {
+    ofstream log("ball_log.txt", ios::app);
+    if (log.is_open()) {
+        log << message << endl;
+        log.close();
+    }
+}
+
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+bool keyPressed() {
+#ifdef _WIN32
+    return _kbhit();
+#else
+    return false;
+#endif
+}
+
+char getKey() {
+#ifdef _WIN32
+    return _getch();
+#else
+    return getchar();
+#endif
+}
 
 int main() {
-    student s;  
-
+    logToFile("Программа запущена");
     
-    cout << "Введите имя: ";
-    cin >> s.name;  
-
-   
-    cout << "Введите возраст: ";
-    cin >> s.age;
-
-    cout << "Введите роль: ";
-    cin >> s.role;
-
-   
-    cout << "Введите группу: ";
-    cin >> s.group;
-
+    float pos = 0;
+    float velocity = 2.5;
+    float gravity = 0.3;
+    float bounce = -0.9;
+    int ground = 15;
+    int speed = 40;
+    bool running = true;
+    int score = 0;
     
-    cout << "\n=== Данные студента ===" << endl;
-    cout << "Имя: " << s.name << endl;
-    cout << "Возраст: " << s.age << endl;
-    cout << "Роль: " << s.role << endl;
-    cout << "Группа: " << s.group << endl;
-
-    return 0; 
+    while (running) {
+        clearScreen();
+        
+        if (keyPressed()) {
+            char key = getKey();
+            if (key == 'q' || key == 'Q') {
+                running = false;
+                logToFile("Программа остановлена");
+            }
+            if (key == ' ') {
+                velocity = 5.0;
+                score++;
+            }
+        }
+        
+        int intPos = (int)pos;
+        if (intPos < 0) intPos = 0;
+        if (intPos > ground) intPos = ground;
+        
+        for (int i = 0; i < ground - intPos; i++) {
+            cout << endl;
+        }
+        
+        cout << "       O";
+        cout << "  Score: " << score << endl;
+        
+        velocity += gravity;
+        pos += velocity;
+        
+        if (pos >= ground) {
+            pos = ground;
+            velocity = velocity * bounce;
+            score++;
+            logToFile("Отскок от пола");
+            
+            if (abs(velocity) < 0.3) {
+                velocity = 0;
+                pos = ground;
+            }
+        }
+        
+        if (pos < 0) {
+            pos = 0;
+            velocity = abs(velocity) * bounce;
+            logToFile("Отскок от потолка");
+        }
+        
+        this_thread::sleep_for(chrono::milliseconds(speed));
+    }
+    
+    clearScreen();
+    cout << "Game Over! Final score: " << score << endl;
+    logToFile("Программа завершена");
+    
+    return 0;
 }
