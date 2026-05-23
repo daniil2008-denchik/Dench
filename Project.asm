f8afcb5401eb4cd09ebd3ab746cc9e76cd0a63d8
daniil2008-denchik <daniilsalahutdinov36@gmail.com>
2026-05-23 21:40:58 +1000

    Update Project.asm

--- Project.asm
+++ Project.asm
@@ -19,15 +19,14 @@ _start:
 loop:
     call    clear_screen
     
-    ; Проверка клавиши (упрощённо)
     call    check_key
     cmp     al, 'q'
     je      exit
     
-    ; Рисование шарика
+    
     call    draw_ball
     
-    ; Физика
+   
     movss   xmm0, [vel]
     addss   xmm0, [grav]     ; vel += gravity
     movss   [vel], xmm0
@@ -36,13 +35,13 @@ loop:
     addss   xmm0, [vel]      ; pos += vel
     movss   [pos], xmm0
     
-    ; Проверка пола
+   
     movss   xmm0, [pos]
     movss   xmm1, [ground]
     comiss  xmm0, xmm1
     jb      check_ceiling
     
-    ; Отскок от пола
+    
     movss   xmm0, [ground]
     movss   [pos], xmm0
     movss   xmm0, [vel]
@@ -52,12 +51,12 @@ loop:
     jmp     delay
 
 check_ceiling:
-    ; Проверка потолка
+    
     xorps   xmm1, xmm1
     comiss  xmm0, xmm1
     jae     delay
     
-    ; Отскок от потолка
+    
     xorps   xmm0, xmm0
     movss   [pos], xmm0
     movss   xmm0, [vel]
@@ -66,7 +65,7 @@ check_ceiling:
     movss   [vel], xmm0
 
 delay:
-    ; Задержка
+    
     mov     eax, [speed]
     call    sleep_ms
     
@@ -74,14 +73,13 @@ delay:
 
 exit:
     call    clear_screen
-    ; Вывод финального счёта
-    ; ...
+    
     mov     eax, 60         ; sys_exit
     xor     edi, edi
     syscall
 
 clear_screen:
-    ; Очистка экрана
+    
     mov     eax, 4          ; sys_write
     mov     edi, 1          ; stdout
     mov     esi, CSI        ; "\033[2J\033[H"
@@ -90,6 +88,5 @@ clear_screen:
     ret
 
 draw_ball:
-    ; Рисование пустых строк + шарика
-    ; ... (сложная логика с циклами)
+    
     ret
